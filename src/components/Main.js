e38fc93fc4ac15bef7c15fd7328b151e90e8e6b0
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-07 22:02:15 -0400

    Fixed filter issue. Removed console.log used for debugging. Added comments to functions.

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,84 +14,72 @@ class Main extends React.Component {
         base_results:[]
     };
 
- // When this component mounts, search the randomuser API for 10 users
- componentDidMount() {
-    this.searchAPI("&results=10");
-  }
-
-  searchAPI = query => {
-    API.search(query)
-      .then(res => {
-        //   console.log(res.data.results);
-          this.setState({ results: res.data.results, base_results: res.data.results })})
-      .catch(err => console.log(err));
-  };
-
-  handleSortChange = event => {
-      console.log('sort change function');
-    //   console.log(event.target.value)
-      const value = event.target.value;
-      this.setState({
-          sortChoice: value, 
-          base_results: this.state.results
-      })
-
-      this.state.results.sort(function(a,b) {
-          if(event.target.value==='last') {
-                if (a.name.last > b.name.last) {
-                    return 1;
-                } else if (a.name.last < b.name.last) {
-                    return -1;
-                } return 0;
-            } else if (event.target.value==='zipcode') {
-                if (a.location.postcode > b.location.postcode) {
-                    return 1;
-                } else if (a.location.postcode < b.location.postcode) {
-                      return -1;
-                }  return 0;
-            } else {
-                if (a[event.target.value] > b[event.target.value]) {
-                    return 1;
-                } else if (a[event.target.value] < b[event.target.value]) {
-                      return -1;
-                }  return 0;
-            }
-
-        })
+    // When this component mounts, search the randomuser API for 10 users
+    componentDidMount() {
+        this.searchAPI("&results=25");
+    }
+    //search randomUser API
+    searchAPI = query => {
+        API.search(query)
+        .then(res => {
+            this.setState({ results: res.data.results, base_results: res.data.results })})
+        .catch(err => console.log(err));
     };
+    //handle change to sort drop-down
+    handleSortChange = event => {
+        console.log('sort change function');
+        const value = event.target.value;
+        this.setState({
+            sortChoice: value, 
+            base_results: this.state.results
+        })
+        //sort data by selected drop-down value
+        this.state.results.sort(function(a,b) {
+            if(event.target.value==='last') {
+                    if (a.name.last > b.name.last) {
+                        return 1;
+                    } else if (a.name.last < b.name.last) {
+                        return -1;
+                    } return 0;
+                } else if (event.target.value==='zipcode') {
+                    if (a.location.postcode > b.location.postcode) {
+                        return 1;
+                    } else if (a.location.postcode < b.location.postcode) {
+                        return -1;
+                    }  return 0;
+                } else {
+                    if (a[event.target.value] > b[event.target.value]) {
+                        return 1;
+                    } else if (a[event.target.value] < b[event.target.value]) {
+                        return -1;
+                    }  return 0;
+                }
 
+            })
+        };
+
+    //handle change to filter drop-down
     handleFilterChange = event => {
         console.log('filter change function');
-        console.log(this.state.base_results);
         const value_filter = event.target.value;
         this.setState({
             filterChoice: value_filter,
-            results: this.state.base_results
         })
         this.filterData(event);
     }
 
+    //filter by gender
     filterData(event) {
         console.log('filterData function called')
-        console.log(event.target.value)
-        console.log(this.state.results)
         if (event.target.value==='female') {
-            const filteredArray = this.state.results.filter(gender => gender.gender === 'female');
-            console.log(filteredArray)
-            this.setState({results: filteredArray}, this.callBack);
+            const filteredArray = this.state.base_results.filter(gender => gender.gender === 'female');
+            this.setState({results: filteredArray});
         } else if (event.target.value==='male') {
-            console.log(this.state.results);
-            const filteredArray = this.state.results.filter(gender => gender.gender === 'male');
-            console.log(filteredArray)
-            this.setState({results: filteredArray}, this.callBack)
+            const filteredArray = this.state.base_results.filter(gender => gender.gender === 'male');
+            this.setState({results: filteredArray})
         } else {
             this.setState({results: this.state.base_results});
         }
-    }
-
-    callBack() {
-        console.log(this.state.results);
-        return;
     }
   
     render() {
